b9a09897a41f5331298f6dedca3357d19cbabd58
Dreamr <dreamr.okelly@gmail.com>
2013-11-14 23:03:21 -0800

    attempting to fix coveralls being weird

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,14 @@
 if RUBY_ENGINE == "ruby"
   begin
+    require 'simplecov'
     require 'coveralls'
-    Coveralls.wear!
     SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
       Coveralls::SimpleCov::Formatter,
       SimpleCov::Formatter::HTMLFormatter
     ]
+    SimpleCov.start do
+      add_filter "/test/"
+    end
   rescue LoadError
     warn "unable to load Coveralls!"
   end
