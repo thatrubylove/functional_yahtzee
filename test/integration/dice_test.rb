81de1014b32c8ad57959a7a4cdedb7f1b43ae765
Dreamr <dreamr.okelly@gmail.com>
2013-11-17 13:56:59 -0800

    refactor and test

---
+++ test/integration/dice_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+require 'dice'
+
+describe "A turn of dice rolling" do
+  subject { Yahtzee::Dice }
+
+  it "will return 5 new dice values" do
+    subject.roll(5).count.must_equal 5
+  end
+
+  it "will play a turn to a large straight" do
+    roll_1 = [1,2,3,6,6]
+
+    roll_2 = subject.reroll(roll_1[0..2])
+    roll_3 = roll_2[0..2] + [4,5]
+
+    roll_3.must_equal (1..5).to_a
+  end
+end
