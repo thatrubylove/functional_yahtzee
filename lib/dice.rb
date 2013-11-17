e469040e758df07ba5a2803f3250994e153b43cf
Dreamr <dreamr.okelly@gmail.com>
2013-11-17 13:46:10 -0800

    added reroll

--- lib/dice.rb
+++ lib/dice.rb
@@ -8,6 +8,11 @@ module Yahtzee::Dice
     limit = num > MAX_DIE_COUNT ? MAX_DIE_COUNT : num
     1.upto(limit).map {|die| rand(1..DIE_FACES) }
   end
+
+  def self.reroll(keepers)
+    new_roll = keepers + roll(MAX_DIE_COUNT-keepers.count)
+    new_roll[0..(MAX_DIE_COUNT-1)]
+  end
 end
 
 
--- test/unit/dice_test.rb
+++ test/unit/dice_test.rb
@@ -5,6 +5,20 @@ require 'dice'
 describe Yahtzee::Dice do
   subject { Yahtzee::Dice }
 
+  describe ".reroll(keepers)" do
+    subject { Yahtzee::Dice.reroll([1,2,3]) }
+
+    it "must return the keepers + the new roll" do
+      subject.slice(0..2).must_equal([1,2,3])
+    end
+
+    it "must always return 5 digits" do
+      [[1,2,3],[1,2],[1,2,3,4], [1,2,3,4,5,6]].each do |keepers|
+        Yahtzee::Dice.reroll(keepers).count.must_equal 5
+      end
+    end
+  end
+
   describe ".roll(num)" do
     1.upto(5).each do |roll|
       it "must retun #{roll} dice" do
