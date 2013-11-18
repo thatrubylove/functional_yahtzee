215a32f3e536d0a5264372d98b117d87fe8b01ef
Dreamr <dreamr.okelly@gmail.com>
2013-11-17 18:02:37 -0800

    pruned game init, not need

--- lib/game.rb
+++ lib/game.rb
@@ -5,15 +5,9 @@ require 'scoring'
 # Possibly the imperative shell, I might not end
 # up even needing it if I am clever
 module Yahtzee
-  class Game
+  module Game
     include Yahtzee::Errors
-    extend Yahtzee::Scoring
-
-    attr_reader :score_card
-
-    def initialize(score_card=ScoreCard.new)
-      @score_card = score_card
-    end 
-
+    extend  Yahtzee::Scoring
+    extend  Yahtzee::Dice
   end
 end
--- test/integration/game_test.rb
+++ test/integration/game_test.rb
@@ -4,32 +4,4 @@ require 'game'
 include Yahtzee
 
 describe "Starting a new game" do
-  it "will provide a functional core for playing yahtzee" do
-    skip
-    score_card = ScoreCard.new
-
-    
-    possible_places = [:aces, :twos, :threes, :fours,
-                       :fives, :sixes, :three_of_a_kind,
-                       :four_of_a_kind, :full_house,
-                       :small_straight, :large_straight,
-                       :yahtzee, :bonus_yahtzee_1,
-                       :bonus_yahtzee_2, :bonus_yahtzee_3,
-                       :chance]
-    places_left = score_card.to_hash.select {|place| place.nil? }
-    
-    roll = Dice.roll(5)
-
-      card_saver = ScoreCard.save(score_card)
-      value_checker = ->(placement, value) { value }
-
-      possible_scores = possible_places.reduce({}) do |hash, key|
-        hash.merge( key => Scoring.score(roll, key, &value_checker) )
-      end.reject {|_,v| v.nil? }
-
-      best_possible_score_key = possible_scores.select {|k,v|
-        v == possible_scores.values.max }.keys[0]
-
-      score_card = Scoring.score(roll, best_possible_score_key, &card_saver)
-  end
 end
