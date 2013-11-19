a144500281d63ade57f98871b41ff3853a6cc864
Dreamr <dreamr.okelly@gmail.com>
2013-11-19 09:33:46 -0800

    commented Round for @zenspider

--- lib/round.rb
+++ lib/round.rb
@@ -1,6 +1,10 @@
 require 'yahtzee'
 
 module Yahtzee
+
+  # I haven't exactly figured out what I want in here, it may 
+  # enforce round rules, it may not. As of now it is a DSL method
+  # to group rounds in a readable way
   class Round
 
     def initialize(&block)
